f44221dbf3227d09d5c446b4c07b9ce33ab8c88b
Dragon-Seeker <timekeeperguild@gmail.com>
2024-07-03 02:05:04 -0500

    Update to 1.20.5

--- src/main/java/io/blodhgarm/anvil_player_heads/AnvilPlayerHeads.java
+++ src/main/java/io/blodhgarm/anvil_player_heads/AnvilPlayerHeads.java
@@ -2,16 +2,16 @@ package io.blodhgarm.anvil_player_heads;
 
 import com.mojang.authlib.GameProfile;
 import io.blodhgarm.anvil_player_heads.mixin.AnvilScreenHandlerAccessor;
-import io.blodhgarm.anvil_player_heads.mixin.SkullBlockEntityAccessor;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.entity.SkullBlockEntity;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtHelper;
 import net.minecraft.screen.AnvilScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.StringHelper;
 import net.minecraft.util.collection.DefaultedList;
 
 import java.util.Map;
@@ -29,13 +29,19 @@ public class AnvilPlayerHeads implements ModInitializer {
     public static boolean canHandleRenaming(PlayerEntity player, DefaultedList<Slot> slots, final String playerName) {
         var left = slots.get(0).getStack();
 
-        if (left.getItem() != Items.PLAYER_HEAD || !slots.get(1).getStack().isEmpty() || !(player instanceof final ServerPlayerEntity serverPlayer) || playerName.isEmpty()) return false;
+        if (left.getItem() != Items.PLAYER_HEAD || !slots.get(1).getStack().isEmpty() || !(player instanceof final ServerPlayerEntity serverPlayer)) return false;
 
         final var playerId = player.getId();
 
+        if(!(StringHelper.isValidPlayerName(playerName)) || playerName.isEmpty()) {
+            ENTITY_ID_TO_LOOKUP_NAME.remove(playerId);
+
+            return false;
+        }
+
         ENTITY_ID_TO_LOOKUP_NAME.put(playerId, playerName);
 
-        SkullBlockEntityAccessor.aph$fetchProfile(playerName).thenAcceptAsync(gameProfile -> {
+        SkullBlockEntity.fetchProfileByName(playerName).thenAcceptAsync(gameProfile -> {
             if(!ENTITY_ID_TO_LOOKUP_NAME.containsKey(playerId) || !ENTITY_ID_TO_LOOKUP_NAME.get(playerId).equals(playerName)) return;
 
             serverPlayer.server.executeSync(() -> {
@@ -45,7 +51,7 @@ public class AnvilPlayerHeads implements ModInitializer {
                     anvilHandler.updateResult();
                 }
             });
-        });
+        }, SkullBlockEntity.EXECUTOR);
 
         return true;
     }
@@ -60,15 +66,14 @@ public class AnvilPlayerHeads implements ModInitializer {
         var output = left.copy();
 
         var bl = profile.isPresent();
-        var nbt = output.getOrCreateNbt();
 
         if (bl) {
-            nbt.putString("SkullOwner", profile.get().getName());
+            output.set(DataComponentTypes.PROFILE, new ProfileComponent(profile.get()));
 
             ((AnvilScreenHandlerAccessor) anvilScreenHandler).aph$getLevelCost().set(1);
+        } else {
+            output.remove(DataComponentTypes.PROFILE);
         }
-
-        output.setNbt(nbt);
 
         slots.get(2).setStack(output);
 
